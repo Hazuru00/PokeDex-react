8b9811dc19388bfb0fa74f879e1f92075e0afd47
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-29 18:03:32 +0000

    xd

--- PokeDev/src/App.tsx
+++ PokeDev/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
 
+
 function App() {
 
   return (
     <>
-      <div>
-        <h1>POKEDEV</h1>
+      <div className='bg-gray-800 justify-center text-red-600 flex items-center pb-10'>
+        <h1 className='jaro-Jaroh1'>POKEDEX</h1>
+
+      </div>
+      <div className='bg-blue-950 bg-cover h-screen'>
+        <p>POKEDEV</p>
+
       </div>
     </>
   )
