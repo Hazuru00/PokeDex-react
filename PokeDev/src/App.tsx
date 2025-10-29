d81801bc69259ac83bec67a3ec272589ed58467d
Moises Marcano <92418515+Hazuru00@users.noreply.github.com>
2025-10-29 18:20:43 +0000

    cambios

--- PokeDev/src/App.tsx
+++ PokeDev/src/App.tsx
@@ -6,11 +6,11 @@ function App() {
   return (
     <>
       <div className='bg-gray-800 justify-center text-red-600 flex items-center pb-10'>
-        <h1 className='jaro-Jaroh1'>POKEDEX</h1>
+        <h1 className="jaro-Jaroh1">POKEDEX XD</h1>
 
       </div>
       <div className='bg-blue-950 bg-cover h-screen'>
-        <p>POKEDEV</p>
+        <p>XD papu Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non placeat quae doloremque repellendus nihil, eligendi expedita tempore quo harum iste nesciunt ipsa est qui voluptate architecto quas numquam ratione! Veritatis!</p>
 
       </div>
     </>
